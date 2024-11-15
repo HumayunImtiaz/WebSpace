c7d15bb36041661ffb47f79191535883f67624d9
HumayunImtiaz <Humayunimtiaz81@gmail.com>
2024-11-14 21:48:30 -0800

    Update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-white text-center py-4">
+      <div> &copy; 2024 || TCJ-Space</div>
+    </div>
+  );
+};
+
+export default Footer;
